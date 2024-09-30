1b4eef9e2846e4ed13f9b325a29468e240622f42
KhanWarden <baryrhan2004@gmail.com>
2024-10-01 01:12:45 +0500

    Deleted files from index, updated .gitignore

--- app/config_template.py
+++
@@ -1,2 +0,0 @@
-""" Rename to config.py and in '<KEY>' use the API from Console Cloud GoogleMaps API """
-API_KEY = '<KEY>'
--- app/parser/__init__.py
+++
@@ -1,4 +0,0 @@
-""" Module that parses json and get required data from it """
-
-from .parse_json import get_departure_time_from_data
-from .parse_teams_data import parse_result
