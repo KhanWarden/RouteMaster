1b69911a2cb436eb7d1b1efd707fdf6501d3225d
KhanWarden <baryrhan2004@gmail.com>
2024-10-10 19:27:57 +0500

    Moved to backend folder

--- backend/algorithm/app/maps.py
+++ backend/algorithm/app/maps.py
@@ -1,9 +1,9 @@
 """ Maps module which calculates distance between locations in a pessimistic way """
 import googlemaps
 from datetime import timedelta
-from algorithm.app.config import API_KEY
-from algorithm.app.events import get_events
-from algorithm.app.parser import get_departure_time_from_data
+from backend.algorithm import API_KEY
+from backend.algorithm.app.events import get_events
+from backend.algorithm.app.parser import get_departure_time_from_data
 
 gmaps = googlemaps.Client(key=API_KEY)
 
