c424fd5bdeffae966b42df439f006ace7563cee2
KhanWarden <baryrhan2004@gmail.com>
2024-10-10 19:20:11 +0500

    Now date needs to be written in events.txt Merged 2 functions in 1

--- algorithm/app/events.py
+++ algorithm/app/events.py
@@ -3,25 +3,17 @@
 from datetime import datetime
 from pathlib import Path
 
-project_folder = Path(__file__).parent.parent
-events_file_path = project_folder / 'app' / 'events.txt'
-
-""" The date that you need """
-fixed_date = datetime(2024, 10, 5)
-
-
-def time_of_event_start(hour, minute):
-    return fixed_date.replace(hour=hour, minute=minute)
-
-
-def time_of_event_end(hour, minute):
-    return fixed_date.replace(hour=hour, minute=minute)
+algorithm_folder = Path(__file__).parent.parent
+events_file_path = algorithm_folder / 'app' / 'events.txt'
 
 
 def load_events_from_txt(filename):
     _events = []
     try:
         with open(filename, 'r') as file:
+            year, month, day = map(int, file.readline().strip().split(','))
+            fixed_date = datetime(year, month, day)
+
             for line in file:
                 name, location, start_time, end_time = line.strip().split(';')
                 lat, lng = map(float, location.split(','))
@@ -31,8 +23,8 @@ def load_events_from_txt(filename):
                 _event = {
                     'name': name.strip(),
                     'location': (lat, lng),
-                    'start_time': time_of_event_start(start_hours, start_minutes),
-                    'end_time': time_of_event_end(end_hours, end_minutes)
+                    'start_time': fixed_date.replace(hour=start_hours, minute=start_minutes),
+                    'end_time': fixed_date.replace(hour=end_hours, minute=end_minutes)
                 }
                 _events.append(_event)
     except FileNotFoundError:
@@ -40,11 +32,6 @@ def load_events_from_txt(filename):
     return _events
 
 
-def sort_events_by_time(_events):
-    _sorted_events = sorted(_events, key=lambda event: event['start_time'])
-    return _sorted_events
-
-
 def get_events():
     events = load_events_from_txt(events_file_path)
-    return sort_events_by_time(events)
+    return sorted(events, key=lambda event: event['start_time'])
